13036a76c538ac9c49138bcaca50857115fc1ae1
John Rees <john@bitsushi.com>
2019-03-05 11:28:58 +0000

    log messages

--- client/socket_adapter.ts
+++ client/socket_adapter.ts
@@ -1,5 +1,11 @@
 import * as SocketIOClient from "socket.io-client";
 
+function log(...messages: any): void {
+  if (process.env.DEBUG) {
+    console.log(messages);
+  }
+}
+
 enum STATE {
   CONNECTING,
   OPEN,
@@ -13,25 +19,75 @@ class SocketAdapter {
   constructor(public url: string, auth?) {
     this.readyState = STATE.CONNECTING;
     this.socket = SocketIOClient.connect(url);
-    console.log("connecting");
+    log("connecting");
 
     this.socket.on("connect", () => {
       this.readyState = STATE.OPEN;
-      console.log("connected");
+      log("connected");
+    });
+
+    this.socket.on("reconnect", () => {
+      this.readyState = STATE.OPEN;
+      log("reconnected");
+    });
+
+    this.socket.on("reconnecting", () => {
+      this.readyState = STATE.CONNECTING;
+      log("reconnecting");
+    });
+
+    this.socket.on("logout", () => {
+      auth.logout();
+      log("logout");
     });
 
     this.socket.on("disconnect", reason => {
       this.readyState = STATE.CLOSED;
       if (reason === "transport closed") {
         this.onclose("stopped");
+        log("disconnected: stopped");
       } else {
         this.onclose("closed");
+        log("disconnected: closed");
       }
     });
   }
 
+  join(room, cb) {
+    this.socket.emit("join", { room }, cb);
+    log("joined room", room);
+  }
+
+  leave(room, cb) {
+    this.socket.emit("leave", { room }, cb);
+    log("left room", room);
+  }
+
+  close(code = 1005, reason?: string) {
+    log("close socket", { code, reason });
+    // this.socket.disconnect(code, reason)
+    this.socket.disconnect();
+  }
+
   onclose(event) {
-    console.log("CLOSED", { event });
+    log("closed socket", { event });
+  }
+
+  send(message) {
+    this.socket.emit("sharedb", message);
+    log("send message", message);
+  }
+
+  onopen(event) {
+    log("opened socket", event);
+  }
+
+  onerror(event) {
+    log("error", event);
+  }
+
+  onmessage(event) {
+    log("received message", event);
   }
 }
 
