206b93356e6ab742ad273bd8ec82b2e355123246
John Rees <john@bitsushi.com>
2019-03-05 00:54:03 +0000

    start adding socket.io and sharedb

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,39 @@ import * as cors from "cors";
 import * as express from "express";
 import * as helmet from "helmet";
 import "reflect-metadata";
+import * as ShareDB from "sharedb";
+import * as IO from "socket.io";
 import { createConnection } from "typeorm";
+import PostgresDB from "./db";
 import routes from "./routes";
+
+// sharedb/socket.io WebSockets API
+
+const db = new PostgresDB({
+  connectionString: process.env.DB_URI,
+  ssl: JSON.parse(process.env.DB_SSL)
+});
+
+const sharedb = ShareDB({
+  db,
+  disableDocAction: true,
+  disableSpaceDelimitedActions: true
+});
+
+console.log("set up sharedb");
+
+const WS_PORT = process.env.SOCKET_API_PORT || 5000;
+const io = IO(WS_PORT, {
+  serveClient: false
+});
+
+io.on("connection", function(socket) {
+  console.log({ socket });
+});
+
+console.log(`socket.io listening on: ws://127.0.0.1:${WS_PORT}`);
+
+// typeorm/express REST API
 
 createConnection()
   .then(async () => {
@@ -17,9 +48,9 @@ createConnection()
 
     app.use("/", routes);
 
-    const port = process.env.PORT || 3000;
-    app.listen(port);
+    const PORT = process.env.REST_API_PORT || 5001;
+    app.listen(PORT);
 
-    console.log(`listening: http://127.0.0.1:${port}`);
+    console.log(`express listening on: http://127.0.0.1:${PORT}`);
   })
   .catch(error => console.error(error));
